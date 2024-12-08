1b1d4381450eebbfc89226e491eec36d7e694f0e
Pedro Serer <42715517+Pedro-Serer@users.noreply.github.com>
2024-12-07 19:24:35 -1200

    Add files via upload

--- main.php
+++ main.php
@@ -2,13 +2,15 @@
     require_once 'classes/jogo.php';
     
     // Processa o primeiro arquivo
-    $arquivo1 = new arquivo1(10, 7);
+    $arquivo1 = new Jogo(10, 7);
 
     $arquivo1->lerArquivo('assets/entrada1.txt');
     $arquivo1->processaEntrada();
 
+    echo "<br>------------------------------------<br>";
+
     // Processa o segundo arquivo
-    $arquivo2 = new arquivo1(5, 6);
+    $arquivo2 = new Jogo(5, 6);
 
     $arquivo2->lerArquivo('assets/entrada2.txt');
     $arquivo2->processaEntrada();
